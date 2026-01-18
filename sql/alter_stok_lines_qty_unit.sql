7a882c92ced29db4362cb0c3c8f0b6073a39dd07
abuarfan <keluargaabuarfan@gmail.com>
2026-01-18 13:39:11 +0700

    Delete sql directory

--- sql/alter_stok_lines_qty_unit.sql
+++
@@ -1,4 +0,0 @@
--- Tambah kolom qty & unit untuk stok_lines (jalankan sekali)
-alter table public.stok_lines
-  add column if not exists qty numeric null,
-  add column if not exists unit text null;
